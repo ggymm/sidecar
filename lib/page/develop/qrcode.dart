ea4fc0cf73b860a3051c3ccc689a80b299370127
gongym <1967988842@qq.com>
2025-04-30 21:56:49 +0800

    update

--- lib/page/develop/qrcode.dart
+++ lib/page/develop/qrcode.dart
@@ -10,9 +10,78 @@ class QrcodeDevelopPage extends StatefulWidget {
 class QrcodeDevelopPageState extends State<QrcodeDevelopPage> {
   @override
   Widget build(BuildContext context) {
-    return ScaffoldPage.scrollable(
+    return ScaffoldPage(
       header: PageHeader(title: Text('二维码')),
-      children: [Text('QrcodeDevelop')],
+      content: Padding(
+        padding: EdgeInsetsDirectional.only(
+          end: PageHeader.horizontalPadding(context),
+          start: PageHeader.horizontalPadding(context),
+          bottom: kPageDefaultVerticalPadding,
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Card(
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text('图片'),
+                              Row(
+                                children: [
+                                  SizedBox(
+                                    width: 120,
+                                    height: 36,
+                                    child: Button(
+                                      child: const Text('选择文件'),
+                                      onPressed: () async {},
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          ),
+                          SizedBox(height: 20),
+                        ],
+                      ),
+                      padding: EdgeInsets.all(20),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  SizedBox(
+                    height: 240,
+                    child: Card(
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [Text('解析结果')],
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          ),
+                          SizedBox(height: 20),
+                          Expanded(
+                            child: TextBox(
+                              maxLines: null,
+                              readOnly: true,
+                              style: TextStyle(fontFamily: 'Cascadia Mono'),
+                            ),
+                          ),
+                        ],
+                      ),
+                      padding: EdgeInsets.all(20),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
